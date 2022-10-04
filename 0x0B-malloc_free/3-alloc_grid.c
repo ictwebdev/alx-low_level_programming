74ea5a77493101754af6f84e9d43e86e8e4e2d15
IctWebDev <ictwebsitesolutions@gmail.com>
2022-10-04 13:29:53 -0700

    c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,45 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * alloc_grid - A function that returns a pointer to a
- * 2 dimensional array of integers
- * @width: An input integer at number of columns
- * @height: An input integer at number of rows
- * Return: pointer to a 2D array, NULL on failure
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
+ * Return: a pointer to a 2 dimensional array of integers
  */
+
 int **alloc_grid(int width, int height)
 {
-int **array, i = 0, j = 0;
+int **grid, row, col;
 
-if (width <= 0 || height <= 0)
+if (width < 1 || height < 1)
 return (NULL);
 
-array = (int **)malloc(sizeof(int *) * height);
-if (array == NULL)
-return (NULL);
-for (; i < height; i++)
+grid = (int **)malloc(height * sizeof(int *));
+
+if (!grid)
 {
-array[i] = (int *)malloc(sizeof(int) * width);
-if (array[i] == NULL)
+free(grid);
 return (NULL);
 }
 
-for (i = 0; i < height; i++)
-for (; j < width; j++)
-array[i][j] = 0;
-return (array);
+for (row = 0; row < height; row++)
+{
+grid[row] = (int *)malloc(width * sizeof(int));
+
+if (!grid[row])
+{
+while (row)
+{
+free(grid[row]);
+row--;
+}
+free(grid);
+return (NULL);
+}
+for (col = 0; col < width; col++)
+grid[row][col] = 0;
+}
+
+return (grid);
 }
