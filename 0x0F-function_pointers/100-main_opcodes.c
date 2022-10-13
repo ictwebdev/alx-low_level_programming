802fbd125cc86c3722aafbdfaaf2c31f89179013
IctWebDev <ictwebsitesolutions@gmail.com>
2022-10-12 18:41:15 -0700

    c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,26 +1,35 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: The job has been finished
+ * main - check the code for Holberton School students.
+ * @argc: amount of args
+ * @argv: arguments
+ * Return: 1 if an error occurred, 0 otherwise
  */
-
 int main(int argc, char *argv[])
 {
+unsigned char *p = (unsigned char *)main;
+
+unsigned int bytes;
+
+int i;
+
 if (argc != 2)
 {
 printf("Error\n");
 exit(1);
 }
-
 if (atoi(argv[1]) < 0)
 {
 printf("Error\n");
 exit(2);
 }
+
+bytes = atoi(argv[1]);
+
+for (i = 0; i < bytes; i++)
+printf("%02x ", *p++);
+printf("\n");
 return (0);
 }
