6a4ea148dd046ad0368563bc574983fb08dcf044
IctWebDev <ictwebsitesolutions@gmail.com>
2022-10-20 16:32:49 -0700

    c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,15 @@
-/**
- * premain - prints before main
- * Return: void
- */
 #include <stdio.h>
-void __attribute__ ((constructor)) premain()
+/**
+ * firstfunc - prints before the main function is executed.
+ *
+ * Return: Always 0.
+ */
+void firstfunc(void) __attribute__ ((constructor));
+
+void firstfunc(void)
 {
-printf("You're beat! and yet, you must allow,\n%s", "I bore my house upon my back!\n");
+char *m;
+
+m = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+printf("%s", m);
 }
